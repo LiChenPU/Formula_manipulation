f1ece2156842523f53bfadcb7b2c0d5374ec462a
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-03-26 14:47:17 -0400

    include data folder and files

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' @title abundant_isotopes
+#' @description This table include the abundant isotope used to calculate exact mass for given formula
+#' @format A data frame with 97 rows and 5 variables:
+#' \describe{
+#'   \item{\code{element}}{character ##}
+#'   \item{\code{isotope}}{character ##}
+#'   \item{\code{mass}}{double ##}
+#'   \item{\code{abundance}}{double ##}
+#'   \item{\code{ratioC}}{integer ##}
+#'}
+#' @details DETAILS
+"abundant_isotopes"
+
+#' @title full_isotopes
+#' @description This table include the abundant isotope used to calculate exact mass for given formula
+#' @format A data frame with 308 rows and 5 variables:
+#' \describe{
+#'   \item{\code{element}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{isotope}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{mass}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{abundance}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{ratioC}}{integer COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"full_isotopes"
