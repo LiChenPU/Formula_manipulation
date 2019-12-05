cec061dbf1f4521ccb19a31cc4c926407eaaca87
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-12-05 14:57:09 -0500

    support vectorization of formula calculation

--- R/my_calculate_formula.R
+++ R/my_calculate_formula.R
@@ -3,148 +3,177 @@
 
 #' my_calculate_formula
 #'
-#' @param formula1 formula1
-#' @param formula2 formula1
+#' @param Formula1 formula1 (could be a vector)
+#' @param Formula2 formula2 (could be a vector)
 #' @param sign 1 for merge two formula, -1 for subtract second from first formula
-#' @param Is_valid if set True, only return formula if the element count is > 0
+#' @param valid_mat if set True, only return formula if the element count is > 0
 #'
-#' @return the merge or subtract formula
+#' @return the merge or subtract formula in vector or matrix; and if valid_mati is true, a validation of whether negative element presents
 #' @export
 #'
-#' @examples my_calculate_formula(formula1 = "C2H4O1S2P1",formula2 = "N1H1O-1",sign = 1,Is_valid = TRUE)
-my_calculate_formula = function(formula1,formula2,sign = 1,Is_valid = TRUE){
+#' @examples my_calculate_formula(Formula1 = "C2H4O1S2P1",Formula2 = "N1H1O-1",sign = 1,Is_valid = TRUE)
+my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALSE){
+  formula2_ls = list()
+  for(i in 1:length(Formula2)){
+    formula2 = Formula2[i]
+    {
+      formula2 <- gsub("D", "[2]H", formula2)
+      ende2 <- nchar(formula2)
+      element2 <- c()
+      number2 <- c()
+      j <- c(1)
+      while (j <= ende2) {
+        if (substr(formula2, j, j) == c("[")) {
+          b <- j
+          while (any(substr(formula2, j, j) == c("]")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          k <- j
+          while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          element2 <- c(element2, substr(formula2, b, m))
+        }
+        if (any(substr(formula2, j, j) == c("-", ".",  "0", "1", "2", "3",
+                                            "4", "5", "6", "7", "8", "9")) != TRUE) {
+          k <- j
+          while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          j <- c(j - 1)
+          element2 <- c(element2, substr(formula2, k, m))
+        }
+        if (any(substr(formula2, j, j) == c("-", ".",  "0", "1", "2", "3",
+                                            "4", "5", "6", "7", "8", "9")) == TRUE) {
+          k <- j
+          while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) ==
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          j <- c(j - 1)
+          number2 <- c(number2, as.numeric(substr(formula2,
+                                                  k, m)))
+        }
+        j <- j + 1
 
-  {
-    formula2 <- gsub("D", "[2]H", formula2)
-    ende2 <- nchar(formula2)
-    element2 <- c()
-    number2 <- c()
-    j <- c(1)
-    while (j <= ende2) {
-      if (substr(formula2, j, j) == c("[")) {
-        b <- j
-        while (any(substr(formula2, j, j) == c("]")) !=
-               TRUE) {
-          j <- c(j + 1)
-        }
-        k <- j
-        while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) !=
-               TRUE) {
-          j <- c(j + 1)
-        }
-        m <- c(j - 1)
-        element2 <- c(element2, substr(formula2, b, m))
       }
-      if (any(substr(formula2, j, j) == c("-", ".",  "0", "1", "2", "3",
-                                          "4", "5", "6", "7", "8", "9")) != TRUE) {
-        k <- j
-        while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) !=
-               TRUE) {
-          j <- c(j + 1)
-        }
-        m <- c(j - 1)
-        j <- c(j - 1)
-        element2 <- c(element2, substr(formula2, k, m))
-      }
-      if (any(substr(formula2, j, j) == c("-", ".",  "0", "1", "2", "3",
-                                          "4", "5", "6", "7", "8", "9")) == TRUE) {
-        k <- j
-        while (any(substr(formula2, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) ==
-               TRUE) {
-          j <- c(j + 1)
-        }
-        m <- c(j - 1)
-        j <- c(j - 1)
-        number2 <- c(number2, as.numeric(substr(formula2,
-                                                k, m)))
-      }
-      j <- j + 1
 
     }
-
+    number2 = number2 * sign
+    formula2_ls[[length(formula2_ls)+1]] = list(element2, number2)
   }
 
-  {
-    formula1 <- gsub("D", "[2]H", formula1)
-    ende2 <- nchar(formula1)
-    elem_all <- c()
-    count_all <- c()
-    j <- c(1)
-    while (j <= ende2) {
-      if (substr(formula1, j, j) == c("[")) {
-        b <- j
-        while (any(substr(formula1, j, j) == c("]")) !=
-               TRUE) {
-          j <- c(j + 1)
+  formula1_ls = list()
+  for(i in 1:length(Formula1)){
+    formula1 = Formula1[i]
+    {
+      formula1 <- gsub("D", "[2]H", formula1)
+      ende2 <- nchar(formula1)
+      elem_all <- c()
+      count_all <- c()
+      j <- c(1)
+      while (j <= ende2) {
+        if (substr(formula1, j, j) == c("[")) {
+          b <- j
+          while (any(substr(formula1, j, j) == c("]")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          k <- j
+          while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          elem_all <- c(elem_all, substr(formula1, b, m))
         }
-        k <- j
-        while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) !=
-               TRUE) {
-          j <- c(j + 1)
+        if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
+                                            "4", "5", "6", "7", "8", "9")) != TRUE) {
+          k <- j
+          while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) !=
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          j <- c(j - 1)
+          elem_all <- c(elem_all, substr(formula1, k, m))
         }
-        m <- c(j - 1)
-        elem_all <- c(elem_all, substr(formula1, b, m))
+        if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
+                                            "4", "5", "6", "7", "8", "9")) == TRUE) {
+          k <- j
+          while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
+                                                 "2", "3", "4", "5", "6", "7", "8", "9")) ==
+                 TRUE) {
+            j <- c(j + 1)
+          }
+          m <- c(j - 1)
+          j <- c(j - 1)
+          count_all <- c(count_all, as.numeric(substr(formula1,
+                                                      k, m)))
+        }
+        j <- j + 1
       }
-      if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
-                                          "4", "5", "6", "7", "8", "9")) != TRUE) {
-        k <- j
-        while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) !=
-               TRUE) {
-          j <- c(j + 1)
+    }
+    formula1_ls[[length(formula1_ls)+1]] = list(elem_all, count_all)
+  }
+
+  formula_mat = matrix("", nrow = length(formula1_ls), ncol = length(formula2_ls))
+  valid_mat = matrix(F, nrow = length(formula1_ls), ncol = length(formula2_ls))
+  for(row_i in 1:length(Formula1)){
+    elem_all = formula1_ls[[row_i]][[1]]
+    count_all = formula1_ls[[row_i]][[2]]
+    for(col_j in 1:length(Formula2)){
+      element2 = formula2_ls[[col_j]][[1]]
+      number2 = formula2_ls[[col_j]][[2]]
+
+
+
+      for(i in 1:length(element2)){
+        if(any(element2[i]==elem_all) == TRUE){
+          count_all[elem_all==element2[i]] = count_all[elem_all==element2[i]]+number2[i]
+        }else{
+          count_all = c(count_all,number2[i])
+          elem_all = c(elem_all,element2[i])
         }
-        m <- c(j - 1)
-        j <- c(j - 1)
-        elem_all <- c(elem_all, substr(formula1, k, m))
       }
-      if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
-                                          "4", "5", "6", "7", "8", "9")) == TRUE) {
-        k <- j
-        while (any(substr(formula1, j, j) == c("-", ".",  "0", "1",
-                                               "2", "3", "4", "5", "6", "7", "8", "9")) ==
-               TRUE) {
-          j <- c(j + 1)
-        }
-        m <- c(j - 1)
-        j <- c(j - 1)
-        count_all <- c(count_all, as.numeric(substr(formula1,
-                                                    k, m)))
+
+      if(any(count_all<0)){
+        valid_mat[row_i,col_j]=F
       }
-      j <- j + 1
 
+      elem_order = order(elem_all)
+      count_all = count_all[elem_order]
+      elem_all = elem_all[elem_order]
+
+      elem_all=elem_all[count_all!=0]
+      count_all=count_all[count_all!=0]
+      formula_all=c()
+      for (i in 1:length(count_all)) {
+        formula_all <- paste0(formula_all, elem_all[i],
+                              count_all[i])
+      }
+      formula_mat[row_i,col_j]=formula_all
     }
-
   }
 
-  number2 = number2 * sign
-  for(i in 1:length(element2)){
-    if(any(element2[i]==elem_all) == TRUE){
-      count_all[elem_all==element2[i]] = count_all[elem_all==element2[i]]+number2[i]
-    }else{
-      count_all = c(count_all,number2[i])
-      elem_all = c(elem_all,element2[i])
-    }
+  if(length(Formula1)==1 & length(Formula2)==1){
+    formula_mat = as.vector(formula_mat)
+    valid_mat = as.vector(valid_mat)
   }
-  if(any(count_all<0)==TRUE & Is_valid){
-    return(F)
-  } else{
-    elem_order = order(elem_all)
-    count_all = count_all[elem_order]
-    elem_all = elem_all[elem_order]
 
-    elem_all=elem_all[count_all!=0]
-    count_all=count_all[count_all!=0]
-    formula_all=c()
-    for (i in 1:length(count_all)) {
-      formula_all <- paste(formula_all, elem_all[i],
-                           count_all[i], sep = "")
-    }
-    return(formula_all)
-  }
+  if(Valid_formula){return(list(formula_mat, valid_mat))}
+  else{return(formula_mat)}
 }
 
-my_calculate_formula("C1H2N3O4", "C3H2B2")
+# my_calculate_formula(c("C1H2","C3H4O2"), "C1O2", sign = 1, Valid_formula=F)
