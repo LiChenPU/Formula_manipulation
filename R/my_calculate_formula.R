c59bd6378b98ab7e7497415b9221ad7495914050
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-12-05 17:01:19 -0500

    fix minor bug

--- R/my_calculate_formula.R
+++ R/my_calculate_formula.R
@@ -78,8 +78,8 @@ my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALS
     {
       formula1 <- gsub("D", "[2]H", formula1)
       ende2 <- nchar(formula1)
-      elem_all <- c()
-      count_all <- c()
+      element1 <- c()
+      number1 <- c()
       j <- c(1)
       while (j <= ende2) {
         if (substr(formula1, j, j) == c("[")) {
@@ -95,7 +95,7 @@ my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALS
             j <- c(j + 1)
           }
           m <- c(j - 1)
-          elem_all <- c(elem_all, substr(formula1, b, m))
+          element1 <- c(element1, substr(formula1, b, m))
         }
         if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
                                             "4", "5", "6", "7", "8", "9")) != TRUE) {
@@ -107,7 +107,7 @@ my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALS
           }
           m <- c(j - 1)
           j <- c(j - 1)
-          elem_all <- c(elem_all, substr(formula1, k, m))
+          element1 <- c(element1, substr(formula1, k, m))
         }
         if (any(substr(formula1, j, j) == c("-", ".",  "0", "1", "2", "3",
                                             "4", "5", "6", "7", "8", "9")) == TRUE) {
@@ -119,24 +119,25 @@ my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALS
           }
           m <- c(j - 1)
           j <- c(j - 1)
-          count_all <- c(count_all, as.numeric(substr(formula1,
+          number1 <- c(number1, as.numeric(substr(formula1,
                                                       k, m)))
         }
         j <- j + 1
       }
     }
-    formula1_ls[[length(formula1_ls)+1]] = list(elem_all, count_all)
+    formula1_ls[[length(formula1_ls)+1]] = list(element1, number1)
   }
 
   formula_mat = matrix("", nrow = length(formula1_ls), ncol = length(formula2_ls))
   valid_mat = matrix(F, nrow = length(formula1_ls), ncol = length(formula2_ls))
   for(row_i in 1:length(Formula1)){
-    elem_all = formula1_ls[[row_i]][[1]]
-    count_all = formula1_ls[[row_i]][[2]]
+    element1 = formula1_ls[[row_i]][[1]]
+    number1 = formula1_ls[[row_i]][[2]]
     for(col_j in 1:length(Formula2)){
       element2 = formula2_ls[[col_j]][[1]]
       number2 = formula2_ls[[col_j]][[2]]
-
+      count_all = number1
+      elem_all = element1
       for(i in 1:length(element2)){
         if(any(element2[i]==elem_all) == TRUE){
           count_all[elem_all==element2[i]] = count_all[elem_all==element2[i]]+number2[i]
@@ -174,6 +175,6 @@ my_calculate_formula = function(Formula1,Formula2,sign = 1, Valid_formula = FALS
   else{return(formula_mat)}
 }
 
-# my_calculate_formula(c("C1H2","C3H4O2"), "C1O2", sign = 1, Valid_formula=F)
 # Formula1 = c("C1H2","C3H4O2")
-# Formula2 = "C1O2"
+# Formula2 = c("C1O2","N1H3","B1O2")
+# my_calculate_formula(Formula1, Formula2)
