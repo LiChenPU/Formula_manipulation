98cedcaa244a255054223dc4fc5b252e80b9fca8
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2020-04-17 15:19:32 +0800

    add robustness handling non formula entry

--- R/formula_mz.R
+++ R/formula_mz.R
@@ -15,6 +15,12 @@
 #'
 formula_mz = function(formula = "C2H4O1", charge = 0,elem_table = lc8::elem_table){
 
+  if(formula == ""){
+    return(0)
+  }
+  if(is.na(formula) | is.null(formula)){
+    return(formula)
+  }
   mz = numeric()
   for(i in 1:length(formula)){
 
